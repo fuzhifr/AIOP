afeabcee6f73e0888438a4f712d72534e49bc37e
narjisse <narjisszaki@gmail.com>
2014-11-22 18:49:08 +0100

    Spring security

---
+++ src/main/java/core/SpringSecurityInitializer.java
@@ -0,0 +1,6 @@
+package core;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
