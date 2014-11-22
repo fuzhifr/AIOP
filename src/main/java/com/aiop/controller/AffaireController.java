9995f94c3f9ddf82eb17eb98a73fabb2b3967da2
FlorianGomez <floriangomez11@gmail.com>
2014-11-22 16:33:19 +0100

    Modif add type mission

--- src/main/java/com/aiop/controller/AffaireController.java
+++ src/main/java/com/aiop/controller/AffaireController.java
@@ -181,7 +181,7 @@ public class AffaireController {
 	 *         
 	 */
 	@RequestMapping(value = "/affaire/{idAffaire}/typeObjet/{idTypeObjet}/typeMissions", method = RequestMethod.POST)
-	public @ResponseBody String createTypeMissionForTypeObjeteInAffaire(
+	public @ResponseBody LigneDevis createTypeMissionForTypeObjeteInAffaire(
 			@PathVariable("idAffaire") long idAffaire,
 			@PathVariable("idTypeObjet") long idTypeObjet, HttpServletRequest request) {
 
@@ -197,7 +197,7 @@ public class AffaireController {
 		long montant=prix*nbObjet;
 		newLigne.setMontantDevis(montant);
 		affaireService.addLigneDevis(idAffaire,newLigne);
-		return "Success";
+		return newLigne;
 	}
 
 	/*
