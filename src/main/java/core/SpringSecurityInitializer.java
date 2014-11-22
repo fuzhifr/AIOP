ca071c981246ef24db91920a75c404dbe1ef604a
narjisse <narjisszaki@gmail.com>
2014-11-22 19:15:35 +0100

    Revert "Spring security"
    This reverts commit afeabcee6f73e0888438a4f712d72534e49bc37e.

--- src/main/java/core/SpringSecurityInitializer.java
+++
@@ -1,6 +0,0 @@
-package core;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
